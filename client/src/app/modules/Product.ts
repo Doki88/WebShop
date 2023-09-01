bfd446ffd4131439d9d09912a7a34d413277eabd
Dejan Prodanovic <Dejan_Prodanovic@epam.com>
2023-09-01 20:05:22 +0200

    End of section 8

---
+++ client/src/app/modules/Product.ts
@@ -0,0 +1,18 @@
+
+Root Interface Name
+export interface Product {
+  id:number,
+  name:string,
+  description:string,
+  price:number,
+  pictureUrl:string;
+  productType:string,
+  productBrand:string
+}
+
+export interface Datum {
+ pageIndex:number;
+ pageSize:number,
+ count:number;
+ data:Datum[];
+}
