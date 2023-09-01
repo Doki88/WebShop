4cfca8d4701b9e4b21f7682fc6f16d6207184f5b
Dejan Prodanovic <Dejan_Prodanovic@epam.com>
2023-09-01 21:06:07 +0200

    Added new file

--- client/src/app/modules/Product.ts
+++ client/src/app/modules/Product.ts
@@ -1,18 +1,16 @@
-
-Root Interface Name
 export interface Product {
-  id:number,
-  name:string,
-  description:string,
-  price:number,
-  pictureUrl:string;
-  productType:string,
-  productBrand:string
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  pictureUrl: string;
+  productType: string;
+  productBrand: string;
 }
 
 export interface Datum {
- pageIndex:number;
- pageSize:number,
- count:number;
- data:Datum[];
+  pageIndex: number;
+  pageSize: number;
+  count: number;
+  data: Datum[];
 }
